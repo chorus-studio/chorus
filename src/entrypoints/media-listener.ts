6fe5b57f190844fd0b7f0672d456c407437d9bf5
cdrani <charlesdrani@gmail.com>
2025-02-11 17:54:31 -0700

    chore: move media-listener setup into main fn

--- src/entrypoints/media-listener.ts
+++ src/entrypoints/media-listener.ts
@@ -1,38 +1,41 @@
-async function setupMediaListener() {
-    function updatePlaybackSettings(playbackSettings: any) {
-        document.dispatchEvent(new CustomEvent('FROM_SPEED_LISTENER', { detail: playbackSettings }))
-    }
-
-    const { chorus_playback_settings } = await chrome.storage.local.get('chorus_playback_settings')
-    const {
-        playbackRate = 1,
-        preservesPitch = true,
-        volume = 1,
-        muted = false
-    } = chorus_playback_settings
-
-    updatePlaybackSettings({ playbackRate, preservesPitch, volume, muted })
-
-    document.addEventListener('FROM_CHORUS_EXTENSION', (event: CustomEvent) => {
-        updatePlaybackSettings(event.detail)
-    })
-
-    chrome.storage.onChanged.addListener((changes) => {
-        for (let [key, { newValue }] of Object.entries(changes)) {
-            if (key === 'chorus_playback_settings') {
-                updatePlaybackSettings({
-                    muted: newValue.muted,
-                    volume: newValue.volume,
-                    playbackRate: newValue.playbackRate,
-                    preservesPitch: newValue.preservesPitch
-                })
-            }
-        }
-    })
-}
-
 export default defineUnlistedScript({
     main() {
+        async function setupMediaListener() {
+            function updatePlaybackSettings(playbackSettings: any) {
+                document.dispatchEvent(
+                    new CustomEvent('FROM_SPEED_LISTENER', { detail: playbackSettings })
+                )
+            }
+
+            const { chorus_playback_settings } = await chrome.storage.local.get(
+                'chorus_playback_settings'
+            )
+            const {
+                playbackRate = 1,
+                preservesPitch = true,
+                volume = 1,
+                muted = false
+            } = chorus_playback_settings
+
+            updatePlaybackSettings({ playbackRate, preservesPitch, volume, muted })
+
+            document.addEventListener('FROM_CHORUS_EXTENSION', (event: CustomEvent) => {
+                updatePlaybackSettings(event.detail)
+            })
+
+            chrome.storage.onChanged.addListener((changes) => {
+                for (let [key, { newValue }] of Object.entries(changes)) {
+                    if (key === 'chorus_playback_settings') {
+                        updatePlaybackSettings({
+                            muted: newValue.muted,
+                            volume: newValue.volume,
+                            playbackRate: newValue.playbackRate,
+                            preservesPitch: newValue.preservesPitch
+                        })
+                    }
+                }
+            })
+        }
         setupMediaListener()
         return true
     }
