f544e10ac8dccef94e7364b190ab5af40991a6a3
cdrani <charlesdrani@gmail.com>
2025-02-14 02:21:23 -0700

    style: add bottom margin to TrackInfo

--- src/lib/components/TrackInfo.svelte
+++ src/lib/components/TrackInfo.svelte
@@ -2,7 +2,7 @@
     import { nowPlaying } from '$lib/stores/now-playing'
 </script>
 
-<div class="flex flex-col gap-0.5 w-full">
+<div class="flex flex-col w-full mb-1">
     <p class="text-base font-bold text-primary">{$nowPlaying.title}</p>
     <p class="text-base text-zinc-400 font-semibold">{$nowPlaying.artist}</p>
 </div>
