5e128c85b33a9bfb19f1b3d7af684d8717ff1040
cdrani <charlesdrani@gmail.com>
2025-02-17 22:26:21 -0700

    style: update HeartButton ui + colours

--- src/lib/components/HeartButton.svelte
+++ src/lib/components/HeartButton.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
     import { onMount } from 'svelte'
     import { Heart } from 'lucide-svelte'
-    import { Button } from '$lib/components/ui/button'
     import { buttonVariants } from '$lib/components/ui/button'
     import * as Tooltip from '$lib/components/ui/tooltip'
 
@@ -17,7 +16,9 @@
     }
 
     function isSpotifyHighlighted() {
-        const button = document.querySelector('div[data-testid="now-playing-widget"] > button[data-encore-id="buttonTertiary"]')
+        const button = document.querySelector(
+            'div[data-testid="now-playing-widget"] > button[data-encore-id="buttonTertiary"]'
+        )
         if (!button) return false
 
         const ariaChecked = button.getAttribute('aria-checked')
@@ -36,11 +37,19 @@
             id="chorus-heart"
             onclick={handleClick}
             aria-label={isLiked ? 'Remove from Liked Songs' : 'Add to Liked Songs'}
-            class={buttonVariants({ variant: 'ghost', size: 'icon', class: 'size-7 bg-transparent hover:bg-transparent px-0 border-none stroke-current [&_svg]:size-[1.125rem]' })}
+            class={buttonVariants({
+                variant: 'ghost',
+                size: 'icon',
+                class: 'size-7 border-none bg-transparent stroke-current px-0 hover:bg-transparent [&_svg]:size-[1.125rem]'
+            })}
         >
-            <Heart size={24} fill={isLiked ? '#1ed760' : 'none'} color={isLiked ? '#1ed760' : 'currentColor'} />
+            <Heart
+                size={24}
+                fill={isLiked ? '#1ed760' : 'none'}
+                color={isLiked ? '#1ed760' : 'currentColor'}
+            />
         </Tooltip.Trigger>
-        <Tooltip.Content class="text-sm bg-background text-white h-6 p-2">
+        <Tooltip.Content class="bg-background p-2 text-sm text-white">
             <p>{isLiked ? 'Remove from Liked Songs' : 'Add to Liked Songs'}</p>
         </Tooltip.Content>
     </Tooltip.Root>
