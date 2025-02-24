292d2481c6678fc51967b07f8f36f38a50a576dd
cdrani <charlesdrani@gmail.com>
2025-02-23 21:45:40 -0700

    feat: add temp snip store

---
+++ src/lib/stores/snip.ts
@@ -0,0 +1,24 @@
+import { writable } from 'svelte/store'
+
+export type Snip = {
+    is_shared: boolean
+    start_time: number
+    end_time: number
+}
+
+function createSnipStore() {
+    const { subscribe, set, update } = writable<Snip | null>(null)
+
+    function reset() {
+        set(null)
+    }
+
+    return {
+        subscribe,
+        set,
+        reset,
+        update
+    }
+}
+
+export const snipStore = createSnipStore()
