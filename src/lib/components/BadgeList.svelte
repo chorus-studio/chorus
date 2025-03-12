c4dc824765ddcdffee348d0d3ee510aa454abb8f
cdrani <charlesdrani@gmail.com>
2025-03-12 17:37:41 -0600

    style: update hover color for badges in BadgeList component

--- src/lib/components/BadgeList.svelte
+++ src/lib/components/BadgeList.svelte
@@ -6,7 +6,7 @@
 <div class="flex w-full items-center justify-between gap-x-0.5">
     {#each list as item}
         <Badge
-            class="cursor-pointer rounded-[2px] bg-slate-700 text-zinc-200 hover:bg-slate-800"
+            class="cursor-pointer rounded-[2px] bg-slate-700 text-zinc-200 hover:bg-slate-600"
             onclick={() => handleSelect(item)}>{item}</Badge
         >
     {/each}
