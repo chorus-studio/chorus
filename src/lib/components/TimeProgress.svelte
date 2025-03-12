0c0119ebaddb56514fb22af6abc5e4cd9ca4d7db
cdrani <charlesdrani@gmail.com>
2025-03-11 21:40:45 -0600

    feat: add playback rate display in time progress component

--- src/lib/components/TimeProgress.svelte
+++ src/lib/components/TimeProgress.svelte
@@ -36,74 +36,87 @@
     $: currentPosition = Math.round($nowPlaying.current ?? 0)
 </script>
 
-<div
-    class="relative mt-1.5 flex h-full w-full {isChorus
-        ? 'gap-x-3'
-        : 'mt-3.5 flex-col'} items-center justify-between"
->
-    {#if isChorus}
-        <span class="min-w-7 text-xs font-semibold text-white">
-            {$nowPlaying.current >= 0 ? formatTime($nowPlaying.current) : '-:--'}
+<div class="relative flex w-full items-center justify-between gap-x-3">
+    {#if isChorus && $nowPlaying.playback}
+        <span class="min-w-7 text-xs font-semibold text-[#1ed760]">
+            {$nowPlaying.playback.playback_rate}x
         </span>
     {/if}
 
     <div
-        role="slider"
-        tabindex={0}
-        bind:this={sliderRef}
-        aria-label="Time slider"
-        class="relative w-full"
-        id={id ?? 'popup-time-progress'}
-        aria-valuenow={currentPosition}
-        onkeydown={setProgress}
-        onmousedown={setProgress}
-        onmousemove={handleMouseMove}
-        onmouseenter={() => (isHovering = true)}
-        onmouseleave={() => (isHovering = false)}
+        class="flex w-full {isChorus ? 'gap-x-3' : 'mt-3.5 flex-col'} items-center justify-between"
     >
-        {#if isHovering}
-            <div
-                class="absolute bottom-full -translate-x-1/2 rounded-sm px-1"
-                style="left: {(hoverPosition / $nowPlaying.duration) * 100}%"
-            >
-                <span class="text-xs font-semibold {textColor} brightness-75">
-                    {formatTime(hoverPosition)}
+        {#if isChorus}
+            <span class="min-w-7 text-xs font-semibold text-white">
+                {$nowPlaying.current >= 0 ? formatTime($nowPlaying.current) : '-:--'}
+            </span>
+        {/if}
+
+        <div
+            role="slider"
+            tabindex={0}
+            bind:this={sliderRef}
+            aria-label="Time slider"
+            class="relative w-full"
+            id={id ?? 'popup-time-progress'}
+            aria-valuenow={currentPosition}
+            onkeydown={setProgress}
+            onmousedown={setProgress}
+            onmousemove={handleMouseMove}
+            onmouseenter={() => (isHovering = true)}
+            onmouseleave={() => (isHovering = false)}
+        >
+            {#if isHovering}
+                <div
+                    class="absolute bottom-full -translate-x-1/2 rounded-sm px-1"
+                    style="left: {(hoverPosition / $nowPlaying.duration) * 100}%"
+                >
+                    <span class="text-xs font-semibold {textColor} brightness-75">
+                        {formatTime(hoverPosition)}
+                    </span>
+                </div>
+            {/if}
+            <Progress
+                segments={$nowPlaying.snip
+                    ? [
+                          {
+                              start: $nowPlaying.snip?.start_time,
+                              end: $nowPlaying.snip?.end_time,
+                              color: '#1ed760'
+                          }
+                      ]
+                    : []}
+                value={currentPosition ?? 0}
+                max={$nowPlaying.duration ?? 100}
+                innerClass="{progressColor} {isChorus ? 'brightness-100' : 'brightness-75'}"
+                class="h-1.5 {isChorus
+                    ? 'rounded-sm'
+                    : 'rounded-none'} border-[0.5px] {borderColor} {bgColor} {isChorus
+                    ? 'brightness-100'
+                    : 'brightness-75'}"
+            />
+        </div>
+
+        {#if isChorus}
+            <span class="min-w-7 text-xs font-semibold text-white">
+                {$nowPlaying.duration ? formatTime($nowPlaying.duration) : '-:--'}
+            </span>
+        {:else}
+            <div class="flex w-full items-center justify-between gap-1">
+                <span class="text-xs font-semibold text-[var(--text)] brightness-75">
+                    {$nowPlaying.current >= 0 ? formatTime($nowPlaying.current) : '-:--'}
+                </span>
+
+                {#if $nowPlaying.playback}
+                    <span class="text-xs font-semibold text-[var(--text)] brightness-75">
+                        {$nowPlaying.playback.playback_rate}x
+                    </span>
+                {/if}
+
+                <span class="text-xs font-semibold text-[var(--text)] brightness-75">
+                    {$nowPlaying.duration ? formatTime($nowPlaying.duration) : '-:--'}
                 </span>
             </div>
         {/if}
-        <Progress
-            segments={$nowPlaying.snip
-                ? [
-                      {
-                          start: $nowPlaying.snip?.start_time,
-                          end: $nowPlaying.snip?.end_time,
-                          color: '#1ed760'
-                      }
-                  ]
-                : []}
-            value={currentPosition ?? 0}
-            max={$nowPlaying.duration ?? 100}
-            innerClass="{progressColor} {isChorus ? 'brightness-100' : 'brightness-75'}"
-            class="h-1.5 {isChorus
-                ? 'rounded-sm'
-                : 'rounded-none'} border-[0.5px] {borderColor} {bgColor} {isChorus
-                ? 'brightness-100'
-                : 'brightness-75'}"
-        />
     </div>
-
-    {#if isChorus}
-        <span class="min-w-7 text-xs font-semibold text-white">
-            {$nowPlaying.duration ? formatTime($nowPlaying.duration) : '-:--'}
-        </span>
-    {:else}
-        <div class="flex w-full items-center justify-between gap-1">
-            <span class="text-xs font-semibold text-[var(--text)]">
-                {$nowPlaying.current ? formatTime($nowPlaying.current) : '-:--'}
-            </span>
-            <span class="text-xs font-semibold text-[var(--text)]">
-                {$nowPlaying.duration ? formatTime($nowPlaying.duration) : '-:--'}
-            </span>
-        </div>
-    {/if}
 </div>
