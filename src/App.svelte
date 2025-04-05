fe3c297b01f3d0adeb582953d8380dc5405d4069
cdrani <charlesdrani@gmail.com>
2025-04-05 17:36:11 -0600

    feat: sync support store on App mount

--- src/App.svelte
+++ src/App.svelte
@@ -5,6 +5,7 @@
     import { nowPlaying } from '$lib/stores/now-playing'
     import { QueueObserver } from '$lib/observers/queue'
     import { trackObserver } from '$lib/observers/track'
+    import { supporterStore } from '$lib/stores/supporter'
     import { playbackObserver } from '$lib/observers/playback'
     import { TracklistObserver } from '$lib/observers/tracklist'
 
@@ -16,6 +17,7 @@
         await nowPlaying.observe()
         await mediaStore.observe()
         playbackObserver.observe()
+        await supporterStore.sync()
     }
 
     onMount(() => {
