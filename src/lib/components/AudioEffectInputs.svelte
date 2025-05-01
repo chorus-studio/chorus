e78d42f9180780e89d0493b807586884dc9538aa
cdrani <charlesdrani@gmail.com>
2025-04-30 20:01:18 -0600

    style: update font-size & spacing of FXEQ ui

--- src/lib/components/AudioEffectInputs.svelte
+++ src/lib/components/AudioEffectInputs.svelte
@@ -29,10 +29,10 @@
     }
 </script>
 
-<div class="flex w-full flex-col gap-y-1">
+<div class="flex w-full flex-col gap-y-2.5">
     <h2 class="text-base leading-none text-gray-400">{type}</h2>
 
-    <div class="flex w-full flex-col gap-y-1">
+    <div class="flex w-full flex-col gap-y-1.5">
         <div class="flex flex-col">
             <Label for="room" class="w-full text-sm text-gray-400">{topLabel}</Label>
             <CustomSelect
@@ -58,9 +58,9 @@
         </div>
     </div>
 
-    <Separator class="mt-1.5 h-0.5 w-full" />
+    <Separator class="mt-1 h-0.5 w-full" />
 
-    <p class="inline-flex w-full justify-between text-base text-gray-400">
-        {type} <span class="justify-end text-base text-white">{$effectsStore[type]}</span>
+    <p class="inline-flex w-full justify-end text-sm text-white">
+        {$effectsStore[type]}
     </p>
 </div>
